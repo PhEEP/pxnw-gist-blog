802b8f43c9a385e35558e56f4ce4402420fcc87b
Philippe Moore <philippealmoore@gmail.com>
2017-12-02 11:09:58 -0800

    Grid of blog poss from gists

--- src/components/GistBlogList.vue
+++ src/components/GistBlogList.vue
@@ -1,12 +1,28 @@
 <template>
   <v-layout row wrap>
+    <v-flex xs12>
+      <h1>{{ githubUser }}</h1>
+    </v-flex>
     <v-flex
       v-for="(gist, index) in gistList"
       :key="index"
+      xs6
+      md3
+      lg2
+
     >
-      <pre>
-        {{gist}}
-      </pre>
+      <v-card
+      :to="'/gist/' + gist.id"
+      >
+        <v-card-title>
+          <h4>
+            {{ gist.description || 'No Description' }}
+          </h4>
+        </v-card-title>
+        <v-card-text>
+          {{ gist.updated_at }}
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import GistBlogList from '@/components/GistBlogList'
+import GistBlogListItem from '@/components/GistBlogListItem'
 
 Vue.use(Router)
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'GistBlogList',
+      component: GistBlogList
+    },
+    {
+      path: '/gist/:id',
+      name: 'GistBlogListItem',
+      component: GistBlogListItem,
+      props: true
     }
   ]
 })
